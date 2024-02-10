601820493f4a4dd6a290cf4b820b774c481c3621
zscud <159593054+zscud@users.noreply.github.com>
2024-02-09 19:15:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const noBtn = document.querySelector(".noBtn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML ="<h2>Correct! Good Answer. I love you so much!</h2>";
   gif.src = "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGkxc3A2ZzJ6Y3ppMGNmMms0NjU3cTh3YzhvYjE4NDVsMjllcGd5OSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Fq00idJgaECCk/giphy.gif";
+  yesBts.style.display = "none";
   noBtn.style.display = "none";
 });
 
