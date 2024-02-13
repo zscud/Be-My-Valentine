6f8b7e5d69a4e665a4ec97ee34d3106db38fe4ed
zscud <159593054+zscud@users.noreply.github.com>
2024-02-12 19:52:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -16,14 +16,3 @@ noBtn.addEventListener("click", () => {
   noBtn.innerHTML = "";
 });
 
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
-
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
-
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
-});
